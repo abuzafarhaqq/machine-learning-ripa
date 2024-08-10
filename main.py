b654a2bfe2ca0f3d6e58df7d6fe17cba1f241240
abuzafarhaqq <abuzafarhaqq@gmail.com>
2024-08-10 10:14:47 +0600

    added normal code into a function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(0, 11):
-    print(f"number is: {i}")
+def range_to_ten():
+    for i in range(0, 11):
+        print(f"number is: {i}")
 
-print("Finished my program")
+
+print("Started My Program")
+range_to_ten()
+print("End of My Program")
